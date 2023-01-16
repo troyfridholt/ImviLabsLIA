47848855405d93a4aa34a1724f2588cbdf5ac361
undeadnerd <lindbergdaniel1988@gmail.com>
2023-01-16 11:17:32 +0100

    header

--- imvilabs_readtest/src/App.js
+++ imvilabs_readtest/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
-import Footer from './components/Footer';
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+<<<<<<< Updated upstream
       <Footer/>
       
+=======
+>>>>>>> Stashed changes
     </div>
   );
 }
