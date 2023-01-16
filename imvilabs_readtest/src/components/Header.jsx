c822926d0897cadb32f2f046df3348fd2c33398c
undeadnerd <lindbergdaniel1988@gmail.com>
2023-01-16 11:02:55 +0100

    header

--- imvilabs_readtest/src/components/Header.jsx
+++ imvilabs_readtest/src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
+import "../Header.css";
 
 export default function Header() {
   return (
-    <Header>
-      <img src="" alt="" />
+    <Header className="header">
       <ul>
         <li>Link 1</li>
         <li>Link 2</li>
