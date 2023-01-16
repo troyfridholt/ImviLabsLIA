e7a8e1579e0cb6c9c2ddfde6eff0f022bae5b7a7
undeadnerd <lindbergdaniel1988@gmail.com>
2023-01-16 10:49:36 +0100

    Create Header.jsx

---
+++ imvilabs_readtest/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <Header>
+      <img src="" alt="" />
+      <ul>
+        <li>Link 1</li>
+        <li>Link 2</li>
+        <li>Link 3</li>
+        <li>Link 4</li>
+      </ul>
+    </Header>
+  );
+}
