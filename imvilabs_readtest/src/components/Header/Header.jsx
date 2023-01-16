dc05dbeea711ddf5153d87c22d40dd139826ff27
undeadnerd <lindbergdaniel1988@gmail.com>
2023-01-16 11:39:24 +0100

    cleaningUp

--- imvilabs_readtest/src/components/Header/Header.jsx
+++ imvilabs_readtest/src/components/Header/Header.jsx
@@ -1,15 +1,7 @@
 import React from "react";
-import "./Header.css";
 
-export default function Header() {
-  return (
-    <Header className="header">
-      <ul>
-        <li>Link 1</li>
-        <li>Link 2</li>
-        <li>Link 3</li>
-        <li>Link 4</li>
-      </ul>
-    </Header>
-  );
+function Header() {
+  return <section>Hello!</section>;
 }
+
+export default Header;
