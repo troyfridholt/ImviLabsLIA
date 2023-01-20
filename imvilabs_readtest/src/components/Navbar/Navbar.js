9c6943eab02872cc300968935a20a103dc75913b
undeadnerd <lindbergdaniel1988@gmail.com>
2023-01-20 12:10:21 +0100

    update Header

--- imvilabs_readtest/src/components/Navbar/Navbar.js
+++ imvilabs_readtest/src/components/Navbar/Navbar.js
@@ -7,16 +7,37 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <ul className="nav-items">
-        {navItems.map((item) => {
-         return <li key={item.id} className={item.className}>
-            <Link linkText={item.title} url={item.path} />
-          </li>;
-        })}
-
-        <Link linkText="Personer vi hjälpt" url="$" />
-        <button type="button">Testa dig gratis</button>
-      </ul>
+      <div className="nav--logo-container">
+        <img
+          className="nav--logo"
+          src="imvilabs_readtest/src/Images/imvi-logo.png"
+          alt="Logo"
+        ></img>
+      </div>
+      <div className="nav--items-container">
+        <div className="nav--links-container">
+          <ul className="nav-items">
+            {navItems.map((item) => {
+              return (
+                <li key={item.id} className={item.className}>
+                  <Link
+                    className="nav--link"
+                    linkText={item.title}
+                    url={item.path}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="nav--cart-icon"></div>
+        <div className="nav--btn-container">
+          <button className="nav--btn" type="button">
+            Testa dig gratis
+          </button>
+        </div>
+        <div className="nav--lang-icon"></div>
+      </div>
     </nav>
   );
 }
